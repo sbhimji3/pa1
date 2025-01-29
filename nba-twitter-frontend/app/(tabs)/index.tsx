30cc41a48fb5844837ae8efc6539b2c6213ea5a6
Saim Bhimji <bhimjisaim@gmail.com>
2025-01-28 20:25:53 -0500

    All changes

--- nba-twitter-frontend/app/(tabs)/index.tsx
+++ nba-twitter-frontend/app/(tabs)/index.tsx
@@ -1,28 +1,38 @@
 import { useEffect, useState } from 'react';
-import { Text, StyleSheet, View, TextInput, Button, Alert } from 'react-native';
-import { postName } from '../api/api'
+import { Text, StyleSheet, View, TextInput, Button,FlatList } from 'react-native';
+import { postSearch, getSearch } from '../api/api'
 
 export default function HomeScreen() {
   const [text, onChangeText] = useState('Enter name');
-  const [responseMessage, setResponseMessage] = useState('');
-  const [welcomeMessage, setWelcomeMessage] = useState('Welcome!');
+
+  const [recentSearches, setRecentSearches] = useState([]);
+  const [searchResults, updatedResults] = useState<any[]>([])
 
   async function handleSubmit() {
-    console.log('Button pressed');
-    alert(`Hello, ${text}`);
-    const response = await postName(text);
+    const response = await postSearch(text);
+
     if (response) {
-      setResponseMessage(`${response.name}: ${response.visits} total visits`);
-      setWelcomeMessage(``);
-    } else {
-      setResponseMessage('Error fetching data');
+      fetchRecentSearches()
+      const articles = await getSearch(text)
+      updatedResults(articles)
+    }
+  }
+
+  async function fetchRecentSearches() {
+    try {
+      const response = await fetch('https://ca15de59-957a-4b6e-9dfc-f158c87aad6d.us-east-1.cloud.genez.io/recent-searches');
+      const data = await response.json();
+      console.log("yo")
+      console.log(data)
+      setRecentSearches(Object.values(data.names)); 
+    } catch (error) {
+      console.error('Error fetching recent searches:', error);
     }
   }
 
   return (
     <View style={styles.titleContainer}>
-      <Text style={styles.text2}>{responseMessage}</Text>
-      <Text style={styles.text}>{welcomeMessage}</Text>
+      <Text style={styles.text}>Search your favorite team!</Text>
       <TextInput 
         style={styles.input} 
         onChangeText={onChangeText} 
@@ -34,6 +44,27 @@ export default function HomeScreen() {
         title="Submit" 
         onPress={handleSubmit} 
       />
+      <View style={styles.recentSearchesContainer}>
+      <Text style={styles.recentSearchesTitle}>Results</Text>
+      <FlatList
+        data={searchResults.slice(0,3)}
+        renderItem={({ item }) => (
+          <Text style={styles.recentSearchText}>
+            {item.source} - {item.title}
+          </Text>
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
+      </View>
+      <View style={styles.recentSearchesContainer}>
+      <Text style={styles.recentSearchesTitle}>Recent Searches</Text>
+      
+      <FlatList
+        data={recentSearches?.slice(0, 3) || []}
+        renderItem={({ item }) => <Text style={styles.recentSearchText}>{item}</Text>}
+        keyExtractor={(item, index) => index.toString()}
+      />
+      </View>
     </View>
   );
 }
@@ -48,8 +79,9 @@ const styles = StyleSheet.create({
     gap: 8,
   },
   text: {
-    fontSize: 72,
+    fontSize: 32,
     fontWeight: 'bold',
+    alignItems: 'center',
     color: '#478'
   },
   text2: {
@@ -62,9 +94,34 @@ const styles = StyleSheet.create({
     borderColor: 'gray',
     borderWidth: 1,
     marginRight: 10,
-    marginTop: 100,
+    marginTop: 40,
     marginBottom: 0,
     paddingLeft: 10,
     width: '70%',
-  }
+  },
+  recentSearchText: {
+    fontSize: 16,
+    padding: 5,
+    backgroundColor: '#fafafa',
+    marginVertical: 5,
+    borderRadius: 25,
+    alignItems: 'center',
+  },
+  recentSearchesContainer: {
+    width: '80%',
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: '#f2f2f2',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    marginBottom: 20,
+  },
+  recentSearchesTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    justifyContent: 'center',
+    color: '#333',
+    marginBottom: 10,
+  },
 });
--- nba-twitter-frontend/app/api/api.js
+++ nba-twitter-frontend/app/api/api.js
@@ -1,8 +1,8 @@
-export const postName = async (name) => {
+export const postSearch = async (name) => {
   const data = { name: name };
 
   try {
-    const response = await fetch('https://ca15de59-957a-4b6e-9dfc-f158c87aad6d.us-east-1.cloud.genez.io/visit', {
+    const response = await fetch('https://ca15de59-957a-4b6e-9dfc-f158c87aad6d.us-east-1.cloud.genez.io/search', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -11,10 +11,28 @@ export const postName = async (name) => {
     });
     console.log(response)
 
-    const json = await response.json(); // Wait for the response to be converted to JSON
-    const { name, visits } = json; // Destructure the response
+    const json = await response.json(); 
+    const { name, visits } = json;
     return { name, visits };
   } catch (error) {
     console.error('Error during POST request:', error);
+  }
+}
+
+export const getSearch = async (name) => {
+  
+  try {
+    const newsResponse = await fetch(`https://ca15de59-957a-4b6e-9dfc-f158c87aad6d.us-east-1.cloud.genez.io/news?text=${name}`);
+    const newsData = await newsResponse.json();
+    
+    const articlesList = newsData.articles.map(article => ({
+      source: article.source.name,
+      title: article.title,
+    }));
+    console.log(articlesList.slice(0,3))
+    return articlesList
+    
+    
+  } catch (error) {
   }
 }
